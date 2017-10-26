7589760e25871aaea7075c8a0f7b4918056e0210
ufalak_globons <ufalak@globons.com>
2017-10-26 19:44:01 -0300

    h

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
     var tobi = { name: 'Travis' };
     memdb.save(tobi);
     var ret = memdb.first({ name: 'Travis' });
-    var welcome = 'Hello ' + ret.name + '!!';
+    var welcome = 'Hello ' + ret.name + '!h!';
     res.end(welcome); 
 }).listen(port);
 console.log('Server running at http://localhost:' + port);
